4b1888d42d5c4c635d5f02f5dba2d0dd0af6dea1
91093467 <91093467@mvusd.net>
2021-11-03 08:25:38 -0700

    Added Minesweeper activity

---
+++ app/src/main/java/com/example/branchpracticej21/Minesweeper.java
@@ -0,0 +1,14 @@
+package com.example.branchpracticej21;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Minesweeper extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_minesweeper);
+    }
+}
