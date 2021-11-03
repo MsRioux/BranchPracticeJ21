fea16f77ece6f9fcce9e932499aaaccb699c6512
91086831 <am2662004@gmail.com>
2021-11-03 08:40:45 -0700

    Added comment to Main Activity

--- app/src/main/java/com/example/branchpracticej21/MainActivity.java
+++ app/src/main/java/com/example/branchpracticej21/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    //Samsung is better than Apple
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
